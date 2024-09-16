3cd332042056e8ab789f239432a13ab1f24a0d79
VojiWest <131270168+VojiWest@users.noreply.github.com>
2024-09-16 20:28:13 +0200

    her

--- main.py
+++ main.py
@@ -7,24 +7,27 @@ app = Ursina()
 camera.orthographic = True
 camera.fov = 10  # Field of view to adjust the zoom level
 
-### Create entities ###
+# Define movement speed of agents
+move_speed = 1
+
+""" CREATE ENTITIES """ # I should probably do this in a better way, but for now this is what I have
 
 # Create a flat 2D platform (background)
 platform = Entity(
     model='quad',  # Use a 2D quad
     scale=(10, 10),
     color=color.pink,
-    position=(0, 0),  # Flat on the x-y plane
+    position=(0, 0, 1),  # Flat on the x-y plane at z=1 (which is further away from the camera) to prevent z-fighting or overlapping with other entities
     texture='white_cube',
-    collider='box'  # Add a box collider to the platform
+    collider='box'  # Add a box collider to the platform to detect collisions
 )
 
 # create a barrier
 barrier = Entity(
     model='quad',
-    scale=(6.0, 0.75),
+    scale=(6.0, 0.75, 0.5), # added z scale (0.5) for testing
     color=color.black,
-    position=(-2, -2),  # Slightly raised to prevent z-fighting
+    position=(-2, -2, -0.01),  # Slightly raised to prevent z-fighting
     collider='box'  # Add a box collider to the square
 )
 
@@ -33,79 +36,83 @@ goal = Entity(
     model='circle',
     scale=(1, 1),
     color=color.green,
-    position=(-3, -3.5),  # Slightly raised to prevent z-fighting
+    position=(-3, -3.5, -0.01),  # Slightly raised to prevent z-fighting
     collider='box'  # Add a box collider to the square
 )
 
-# Create a square on the platform (using quad as a 2D square)
+# Create a square (payload) on the platform (using quad as a 2D square)
 square = Entity(
     model='quad',
     color=color.blue,
     scale=1,
-    position=(2, 2),  # Slightly raised to prevent z-fighting
+    position=(2, 2, -0.01),  # Slightly raised to prevent z-fighting
     collider='box'  # Add a box collider to the square
 )
 
+### Create Agents ###
+
 # Create a circle on the platform
 circle = Entity(
-    model='circle',
+    model='cube',
     color=color.white,
     scale=0.2,
-    position=(-2, 3),  # Slightly raised to prevent z-fighting
+    position=(-2, 3, -0.01),  # Slightly raised to prevent z-fighting
     collider='sphere'  # Add a box collider to the square
 )
 
 # Create another circle on the platform
 circle1 = Entity(
-    model='circle',
+    model='cube',
     color=color.white,
     scale=0.2,
-    position=(-3, 4),  # Slightly raised to prevent z-fighting
+    position=(-3, 4, -0.01),  # Slightly raised to prevent z-fighting
     collider='sphere'  # Add a box collider to the square
 )
 
 # Create another circle on the platform
 circle2 = Entity(
-    model='circle',
+    model='cube',
     color=color.white,
     scale=0.2,
-    position=(-2, 4),  # Slightly raised to prevent z-fighting
+    position=(-2, 4, -0.01),  # Slightly raised to prevent z-fighting
     collider='sphere'  # Add a box collider to the square
 )
 
 # Create another circle on the platform
 circle3 = Entity(
-    model='circle',
+    model='cube',
     color=color.white,
     scale=0.2,
-    position=(3, 4),  # Slightly raised to prevent z-fighting
+    position=(3, 4, -0.01),  # Slightly raised to prevent z-fighting
     collider='sphere'  # Add a box collider to the square
 )
 
 # Create another circle on the platform
 circle4 = Entity(
-    model='circle',
+    model='cube',
     color=color.white,
     scale=0.2,
-    position=(1, 4),  # Slightly raised to prevent z-fighting
+    position=(1, 4, -0.01),  # Slightly raised to prevent z-fighting
     collider='sphere'  # Add a box collider to the square
 )
 
-# Define movement speed
-move_speed = 1
 
 class Barrier():
     def __init__(self, direction):
         self.direction = direction
 
     def update(self):
+        # Reverse the direction of the barrier
         self.direction = self.direction * -1
 
 # Initialize the barrier
 barrier_object = Barrier(0.5)
 
 def keep_in_bounds(agents, square):
+    ### Used to keep the agents and square within the platform bounds ###
+    ### Given list of agents and square, check if they are within the platform bounds and push them back in if they are outside ###
 
+    # Get the bounds of the square and the platform
     top_square = square.y + square.scale_y / 2
     bottom_square = square.y - square.scale_y / 2
     right_square = square.x + square.scale_x / 2
@@ -116,6 +123,7 @@ def keep_in_bounds(agents, square):
     right_platform = platform.x + platform.scale_x / 2
     left_platform = platform.x - platform.scale_x / 2
 
+    # Keep the agents within the platform bounds
     for agent in agents:
         top_agent = agent.y + agent.scale_y / 2
         bottom_agent = agent.y - agent.scale_y / 2
@@ -131,17 +139,7 @@ def keep_in_bounds(agents, square):
         elif left_agent < left_platform:
             agent.x = left_platform + agent.scale_x / 2
 
-    top_square = square.y + square.scale_y / 2
-    bottom_square = square.y - square.scale_y / 2
-    right_square = square.x + square.scale_x / 2
-    left_square = square.x - square.scale_x / 2
-
-    top_platform = platform.y + platform.scale_y / 2
-    bottom_platform = platform.y - platform.scale_y / 2
-    right_platform = platform.x + platform.scale_x / 2
-    left_platform = platform.x - platform.scale_x / 2
-
-    # Check if the square is outside the platform
+    # Check if the square is outside the platform, if so, push it back in
     if top_square > top_platform:
         square.y = top_platform - square.scale_y / 2
     elif bottom_square < bottom_platform:
@@ -165,135 +163,158 @@ def get_angle_between_two_lines(line1, line2):
     angle = math.degrees(math.atan(abs((m2 - m1) / (1 + m1 * m2))))
     return angle
 
-def search_cone(agent, square, cone_angle, cone_length):
+def get_facing_vector(agent, length):
+    ### Get a vector pointing in the direction the agent is facing ###
+    ### Given an agent and a length, return a vector pointing in the direction the agent is facing with the given length ###
+
     facing_direction = agent.transform[1]
-    distance = Vec3(square.x - agent.x, square.y - agent.y, 0).length()
+    z_angle = facing_direction[2]
+    dx = length * math.cos(math.radians(z_angle))
+    dy = length * math.sin(math.radians(z_angle))
+    return Vec3(dx, dy, -0.2).normalized() # Normalize the vector to get a unit vector and set z to -0.2 to prevent z-fighting
+
+def get_face_and_target_lines(agent, target, cone_length):
+    ### Get the lines representing the facing direction of the agent and the line to the target (payload) ###
+    ### Given an agent, target, and cone length, return the lines representing the facing direction of the agent and the line to the target ###
+
+    facing_direction = agent.transform[1]
+
+    # line of facing direction
+    face_line_1 = agent.position
+    z_angle = facing_direction[2]
+    dx = cone_length * math.cos(math.radians(z_angle))
+    dy = cone_length * math.sin(math.radians(z_angle))
+    x_new = agent.x + dx
+    y_new = agent.y + dy
+    face_line_2 = Vec3(x_new, y_new, z_angle)
+    face_line = [face_line_1, face_line_2]
+
+    # line to square
+    target_line_1 = agent.position
+    target_line_2 = Vec3(target.x, target.y, 0)
+    target_line = [target_line_1, target_line_2]
+
+    return face_line, target_line
+
+def search_cone(agent, target, cone_angle, cone_length, payload=True):
+    ### For a given agent, check if the target is within the cone of vision ###
+    ### Given an agent, target, cone angle, and cone length, return True if the target is within the cone of vision, False otherwise ###
+
+    distance = Vec3(target.x - agent.x, target.y - agent.y, 0).length()
     detected = False
+
     if distance < cone_length:
-        # line of facing direction
-        face_line_1 = agent.position
-        z_angle = facing_direction[2]
-        dx = cone_length * math.cos(math.radians(z_angle))
-        dy = cone_length * math.sin(math.radians(z_angle))
-        x_new = agent.x + dx
-        y_new = agent.y + dy
-        face_line_2 = Vec3(x_new, y_new, z_angle)
-        face_line = [face_line_1, face_line_2]
-        # line to square
-        square_line_1 = agent.position
-        square_line_2 = Vec3(square.x, square.y, 0)
-        square_line = [square_line_1, square_line_2]
-
-        print("Face Line: ", face_line, " Square Line: ", square_line)
-
+        # get the lines for the face and the target
+        face_line, target_line = get_face_and_target_lines(agent, target, cone_length)
         # calculate angle between two lines
-        angle = get_angle_between_two_lines(face_line, square_line)
-        # print("FD: ", facing_direction, " Angle: ", angle)
+        angle = get_angle_between_two_lines(face_line, target_line)
         if angle < cone_angle:
-            print("Square detected")
-            agent.color = color.green
+            if payload == True:
+                agent.color = color.green
             detected = True
             return detected
     if not detected:
         agent.color = color.red
-        # rotate the agent and move it a bit 
-        new_rotation = (agent.rotation_z + 10) % 360
+        # rotate the agent 5 degrees
+        new_rotation = (agent.rotation_z + 5) % 360 # keep the rotation within 0-360
         agent.rotation_z = new_rotation
-        # print new facing direction
-        # facing_direction = agent.forward
-        # print(agent.transform[1])
         return detected
 
 def search_for_box(agent, square):
-    facing_direction = agent.transform[1]
-    print(facing_direction)
     # Check for square within cone of vision
-    found = search_cone(agent, square, 45, 100)
+    cone_angle = 5
+    cone_length = 50
+    found = search_cone(agent, square, cone_angle, cone_length)
 
     return found
 
-def move_agents_keyboard(agents, keys):
-    # Move the circle with WASD keys
-    # move = Vec3(0, 0, 0)  # Initial movement vector
-    # move2 = Vec3(0, 0, 0)  # Initial movement vector
-    # keys_clicked = []
-    # if held_keys['a']:  # Move left
-    #     move.x = -time.dt * move_speed
-    #     keys_clicked.append("a")
-    # if held_keys['d']:  # Move right
-    #     move.x = time.dt * move_speed
-    #     keys_clicked.append("d")
-    # if held_keys['w']:  # Move up
-    #     move.y = time.dt * move_speed
-    #     keys_clicked.append("w")
-    # if held_keys['s']:  # Move down
-    #     move.y = -time.dt * move_speed
-    #     keys_clicked.append("s")
+def reach_payload(agent, payload, threshold = 2.5):
+    ### Check if the agent has reached the payload ###
+    ### Given an agent, payload, and threshold, return True if the agent has reached the payload within the threshold distance, False otherwise ###
 
-    # if held_keys['j']:  # Move left
-    #     move2.x = -time.dt * move_speed
-    #     keys_clicked.append("j")
-    # if held_keys['l']:  # Move right
-    #     move2.x = time.dt * move_speed
-    #     keys_clicked.append("l")
-    # if held_keys['i']:  # Move up
-    #     move2.y = time.dt * move_speed
-    #     keys_clicked.append("i")
-    # if held_keys['k']:  # Move down
-    #     move2.y = -time.dt * move_speed
-    #     keys_clicked.append("k")
+    distance = Vec3(payload.x - agent.x, payload.y - agent.y, 0).length()
+    if distance < threshold:
+        return True
+    return False
 
-    # # Update the circle position
-    # circle.position += move
-    # circle2.position += move2
-    # moves = [move, move2]
-    moves = [Vec3(0, 0, 0), Vec3(0, 0, 0)]
+def get_main_direction(square_direction):
+    # Check which component of the direction vector is larger and set the max_square_direction to that
+    # This prevents the sticky behavior when the agent reaches the payload
 
-# Update function to move the circle with WASD keys
+    max_square_direction = Vec3(0, 0, 0)
+    if abs(square_direction.x) > abs(square_direction.y):
+        max_square_direction.x = square_direction.x
+    else:
+        max_square_direction.y = square_direction.y
+
+    return max_square_direction
+
+def move_agent_to_payload(agent, square, barrier):
+    ### Move the agent towards the payload ###
+    square_direction = Vec3(square.x - agent.x, square.y - agent.y, 0) # Calculate the direction vector to the payload
+    agent.position += square_direction.normalized() * time.dt * move_speed # Move the agent towards the payload
+    movement = square_direction.normalized() * time.dt * move_speed # Calculate the movement vector (for later use if we need to move back since we hit an obstacle)
+
+    # Check if the agent intersects with the payload
+    if agent.intersects(square).hit:
+        max_square_direction = get_main_direction(square_direction) # Calculate the push direction based on the movement vector (to prevent sticky behavior)
+
+        # Calculate the push direction based on the movement vector
+        square.position += max_square_direction * time.dt * move_speed
+        if square.intersects(barrier).hit: # If the payload intersects with the barrier after moving, move it back
+            square.position -= max_square_direction * time.dt * move_speed
+
+        # Check for collision again to prevent overlapping
+        if agent.intersects(square).hit: # If the agent intersects with the payload after moving, move it back
+            agent.position -= movement
+
+    return movement
+
+# Update function called every frame
 def update():
 
-    if circle.intersects(goal).hit:
+    # Check if the square (payload) has reached the goal
+    if square.intersects(goal).hit:
         print("Success")
 
-    # Check for collision with the square and push the square away
     for index, agent in enumerate([circle, circle1, circle2, circle3, circle4]):
-        # print("searching")
-        found = search_for_box(agent, square)
-        if found:
-            square_direction = Vec3(square.x - agent.x, square.y - agent.y, 0)
-            agent.position += square_direction.normalized() * time.dt * move_speed
-            movement = square_direction.normalized() * time.dt * move_speed
-            if agent.intersects(square).hit:
-                max_square_direction = Vec3(0, 0, 0)
-                if abs(square_direction.x) > abs(square_direction.y):
-                    max_square_direction.x = square_direction.x
+        # Check if the agent has can see the payload within its cone of vision
+        can_see_payload = search_for_box(agent, square)
+
+        # Raycast to detect obstacles (can ignore this for now, i'm just testing)
+        origin = agent.world_position
+        origin.z = 0.5
+        direction = get_facing_vector(agent, 40)
+        ray = raycast(origin, direction, distance=200, debug=True)
+        # print(agent.transform)
+        # print("Agent", index, "ray", ray.entity, "direction", direction)
+
+        if can_see_payload:
+            # Check if agent reached the payload
+            reached = reach_payload(agent, square)
+            if reached:
+                # Check if goal is occluded (still working on this)
+                # clear = search_cone(agent, goal, 360, 50, False)
+                clear = True
+                if clear:
+                     = move_agent_to_payload(agent, square, barrier)
                 else:
-                    max_square_direction.y = square_direction.y
+                    # move around the the payload (code below is a placeholder) - still working on this
+                    square.position += Vec3(0, 0, 0)
+            else:  
+                # Agent has not reached the payload yet so move towards it 
+                ### Move the agent towards the payload ###
+                movement = move_agent_to_payload(agent, square, barrier)
 
-                # Calculate the push direction based on the movement vector
-                # push_direction = move.normalized()  # Use the direction of movement
-                square.position += max_square_direction * time.dt * move_speed
-                if square.intersects(barrier).hit:
-                    square.position -= max_square_direction * time.dt * move_speed
+        # Check for if agent is colliding with the barrier
+        if agent.intersects(barrier).hit: # If the agent intersects with the barrier, move it back
+            agent.position -= movement
 
-                # Check for collision again to prevent overlapping
-                if agent.intersects(square).hit:
-                    # print('Overlapping detected!')
-                    # set circle position to the edge of the square
-                    # agent.position -= moves[index]
-                    agent.position -= movement
-
-            # Check for collision with the barrier
-            if agent.intersects(barrier).hit:
-                # Calculate the push direction based on the movement vector
-                agent.position -= movement
-
-    # Keep the circle and square within the platform bounds
+    # Keep the agents and square within the platform bounds
     keep_in_bounds([circle, circle1, circle2, circle3, circle4], square)
 
-    # search_for_box([circle, circle2], square)
 
-    # Slide barrier
+    # Slide barrier side to side
     barrier_speed = 0.5
     if barrier.position.x > 2:
         barrier_object.update()
