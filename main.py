03bd135b6b5151a41848ebf3bf2ed2b9617ccaa1
VojiWest <131270168+VojiWest@users.noreply.github.com>
2024-09-18 16:07:07 +0200

    kinda got occlusion working

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ platform = Entity(
 
 # create a barrier
 barrier = Entity(
-    model='quad',
+    model='cube',
     scale=(6.0, 0.75, 0.5), # added z scale (0.5) for testing
     color=color.black,
     position=(-2, -2, -0.01),  # Slightly raised to prevent z-fighting
@@ -33,8 +33,8 @@ barrier = Entity(
 
 # Create goal
 goal = Entity(
-    model='circle',
-    scale=(1, 1),
+    model='cube',
+    scale=(2, 2),
     color=color.green,
     position=(-3, -3.5, -0.01),  # Slightly raised to prevent z-fighting
     collider='box'  # Add a box collider to the square
@@ -42,9 +42,9 @@ goal = Entity(
 
 # Create a square (payload) on the platform (using quad as a 2D square)
 square = Entity(
-    model='quad',
+    model='cube',
     color=color.blue,
-    scale=1,
+    scale=(1.2,1.2),  # Scale the square to be smaller
     position=(2, 2, -0.01),  # Slightly raised to prevent z-fighting
     collider='box'  # Add a box collider to the square
 )
@@ -182,7 +182,15 @@ def get_facing_vector(agent, length):
     z_angle = facing_direction[2]
     dx = length * math.cos(math.radians(z_angle))
     dy = length * math.sin(math.radians(z_angle))
-    return Vec3(dx, dy, -0.2).normalized() # Normalize the vector to get a unit vector and set z to -0.2 to prevent z-fighting
+    return Vec3(dx, dy, -0.2) #.normalized() # Normalize the vector to get a unit vector and set z to -0.2 to prevent z-fighting
+
+def get_vec_at_angle(angle, length):
+    ### Get a vector pointing in the given angle ###
+    ### Given an angle and a length, return a vector pointing in the given angle with the given length ###
+
+    dx = length * math.cos(math.radians(angle))
+    dy = length * math.sin(math.radians(angle))
+    return Vec3(dx, dy, 0) #.normalized() # Normalize the vector to get a unit vector and set z to -0.2 to prevent z-fighting
 
 def get_face_and_target_lines(agent, target, cone_length):
     ### Get the lines representing the facing direction of the agent and the line to the target (payload) ###
@@ -221,25 +229,25 @@ def search_cone(agent, target, cone_angle, cone_length, payload=True):
         angle = get_angle_between_two_lines(face_line, target_line)
         if angle < cone_angle:
             if payload == True:
-                agent.color = color.green
+                agent.color = hsv(190, 0.9, 1)
             detected = True
             return detected
     if not detected:
-        agent.color = color.red
+        agent.color = hsv(350, 0.8, 1)
         # rotate the agent 5 degrees
         new_rotation = (agent.rotation_z + 5) % 360 # keep the rotation within 0-360
         agent.rotation_z = new_rotation
         return detected
 
-def search_for_box(agent, square):
-    # Check for square within cone of vision
+def search_for_entity(agent, target):
+    # Check for target within cone of vision
     cone_angle = 40
     cone_length = 50
-    found = search_cone(agent, square, cone_angle, cone_length)
+    found = search_cone(agent, target, cone_angle, cone_length)
 
     return found
 
-def reach_payload(agent, payload, threshold = 2.5):
+def reach_payload(agent, payload, threshold = 1):
     ### Check if the agent has reached the payload ###
     ### Given an agent, payload, and threshold, return True if the agent has reached the payload within the threshold distance, False otherwise ###
 
@@ -259,6 +267,16 @@ def get_main_direction(square_direction):
         max_square_direction.y = square_direction.y
 
     return max_square_direction
+
+def slide_barrier(barrier, barrier_speed):
+    # Slide barrier side to side
+    barrier_speed = 0.5
+    if barrier.position.x > 2:
+        barrier_object.update()
+    elif barrier.position.x < -2:
+        barrier_object.update()
+    barrier_direction = barrier_object.direction
+    barrier.position += Vec3(time.dt * barrier_speed * barrier_direction, 0, 0)
 
 def move_agent_to_payload(agent, square, barrier):
     ### Move the agent towards the payload ###
@@ -281,6 +299,87 @@ def move_agent_to_payload(agent, square, barrier):
 
     return movement
 
+def get_distance_between_two_3D_points(point1, point2):
+    # Calculate the Euclidean distance between two 3D points
+    return math.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2 + (point1.z - point2.z)**2)
+
+def get_closest_entities(agent, agent_index, angle_jump=5, distance=50):
+    # Raycast around 360 degrees to find the closest entities at each angle
+    found_entities = []
+    for angle_inc in range(0, int(360/angle_jump)):
+        # rotate the agent to the angle
+        new_rotation = (agent.rotation_z + angle_jump * angle_inc) % 360 # keep the rotation within 0-360
+        # print("Agent", agent_index, "rotated to", agent.rotation_z, end=" ")
+        origin = agent.world_position
+        origin.z = -0.1
+        direction = get_vec_at_angle(new_rotation, 1)
+        print("Origin", origin, "Direction", direction)
+        to_ignore = [circle, circle1, circle2, circle3, circle4]
+        ray = raycast(origin, direction, distance=distance, ignore=to_ignore, debug=True)
+
+
+        scales_n_distances = []
+        current_position = agent.position
+        # print("Agent found ", len(ray.entities), "entities at angle", new_rotation)
+        if len(ray.entities) > 0:
+            # print("Agent", index, "\r")
+            for entity in ray.entities:
+                other_position = entity.position
+                distance = get_distance_between_two_3D_points(current_position, other_position)
+                scales_n_distances.append((entity.scale, distance))
+                # print("Scale", entity.scale, "Distance", round(distance, 2))
+                if entity.scale == goal.scale:
+                    print("Agent", agent_index, "found goal at", new_rotation, "degrees")
+                # print("Distance to entity", entity.scale, "is", distance)
+            # print("Agent ", agent_index, "found ", len(ray.entities), "entities")
+
+            closest_entity = None
+            closest_distance = 1000
+            # print("Scales and distances", scales_n_distances)
+            for scale, distance in scales_n_distances:
+                if distance < closest_distance:
+                    closest_distance = distance
+                    closest_entity = scale
+
+            if closest_entity == square.scale:
+                # print("Agent", agent_index, "is closest to the payload")
+                found_entities.append("payload")
+            # if closest_entity == barrier.scale:
+            #     # print("Agent", agent_index, "is closest to the barrier")
+            #     found_entities.append("barrier")
+            if closest_entity == goal.scale:
+                print("Agent", agent_index, "is closest to the goal")
+                # print("Agent", agent_index, "is closest to the goal", scales_n_distances)
+                found_entities.append("goal")
+
+    # print("Agent", agent_index, "found entities", found_entities)
+    return found_entities
+
+def not_ideal_get_closest_entities(agent):
+    # Cast ray in direction of goal not in direction of agent facing
+    origin = agent.world_position
+    origin.z = -0.1
+    direction_goal = Vec3(goal.x - agent.x, goal.y - agent.y, 0).normalized()
+    to_ignore = [circle, circle1, circle2, circle3, circle4]
+    ray = raycast(origin, direction_goal, distance=50, ignore=to_ignore, debug=True)
+    finds = []
+    for entity in ray.entities:
+        distance = get_distance_between_two_3D_points(agent.position, entity.position)
+        finds.append((entity.scale, distance))
+
+    closest_entity = None
+    closest_distance = 1000
+    for scale, distance in finds:
+        if distance < closest_distance:
+            closest_distance = distance
+            closest_entity = scale
+
+    if closest_entity == goal.scale:
+        return "goal"
+    if closest_entity == square.scale:
+        return "payload"
+    return None
+
 # Update function called every frame
 def update():
 
@@ -290,15 +389,7 @@ def update():
 
     for index, agent in enumerate([circle, circle1, circle2, circle3, circle4]):
         # Check if the agent has can see the payload within its cone of vision
-        can_see_payload = search_for_box(agent, square)
-
-        # Raycast to detect obstacles (can ignore this for now, i'm just testing)
-        origin = agent.world_position
-        origin.z = 0.5
-        direction = get_facing_vector(agent, 40)
-        ray = raycast(origin, direction, distance=200, debug=True)
-        # print(agent.transform)
-        # print("Agent", index, "ray", ray.entity, "direction", direction)
+        can_see_payload = search_for_entity(agent, square)
 
         if can_see_payload:
             # Check if agent reached the payload
@@ -306,12 +397,16 @@ def update():
             if reached:
                 # Check if goal is occluded (still working on this)
                 # clear = search_cone(agent, goal, 360, 50, False)
-                clear = True
-                if clear:
+                # found_entities = get_closest_entities(agent, index, angle_jump=5, distance=50)
+                found_entities = not_ideal_get_closest_entities(agent)
+                if found_entities != "goal":
+                    # print("Goal is occluded for agent", index)
                     movement = move_agent_to_payload(agent, square, barrier)
                 else:
+                    print("Goal is visible for agent", index)
                     # move around the the payload (code below is a placeholder) - still working on this
                     square.position += Vec3(0, 0, 0)
+                    movement = Vec3(0, 0, 0)
             else:  
                 # Agent has not reached the payload yet so move towards it 
                 ### Move the agent towards the payload ###
@@ -324,15 +419,8 @@ def update():
     # Keep the agents and square within the platform bounds
     keep_in_bounds([circle, circle1, circle2, circle3, circle4], square)
 
-
     # Slide barrier side to side
-    barrier_speed = 0.5
-    if barrier.position.x > 2:
-        barrier_object.update()
-    elif barrier.position.x < -2:
-        barrier_object.update()
-    barrier_direction = barrier_object.direction
-    barrier.position += Vec3(time.dt * barrier_speed * barrier_direction, 0, 0)
+    slide_barrier(barrier, 0.5)
 
 
 # Run the Ursina application
